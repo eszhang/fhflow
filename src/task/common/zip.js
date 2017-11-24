b0420076d01e99476e158a0b2fed0d6ec8cd88eb
wf <742406475@qq.com>
2017-11-24 14:58:13 +0800

    提交模块打包

--- src/task/common/zip.js
+++ src/task/common/zip.js
@@ -5,9 +5,9 @@ module.exports = function(zipObj, cb){
         year = date.getFullYear(),
         month = date.getMonth() + 1,
         day = date.getDate(),
-        month = month > 9 ? month : '0' + month;
-        day = day > 9 ? day : '0' + day;
-    var name = zipObj.projectName + '-frontend-install-runtime-' + zipObj.version + '-as6u3-x86_64-' + year + month + day + '.zip'
+        month = month > 9 ? month : '0' + month,
+        day = day > 9 ? day : '0' + day,
+        name = zipObj.projectName + '-frontend-install-runtime-' + zipObj.version + '-as6u3-x86_64-' + year + month + day + '.zip';
     gulp.src(zipObj.src)
         .pipe(zip(name))
         .pipe(gulp.dest('')) .on('end',function(){
