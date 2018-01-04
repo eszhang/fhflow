41be7bd87a7ec397c80641693ef9596e082763b6
wf <742406475@qq.com>
2018-01-04 09:53:51 +0800

    删除本下载

--- src/app/redux/data/doc-list.js
+++ src/app/redux/data/doc-list.js
@@ -7,43 +7,31 @@ const docListData = [
     {
         title: 'oasis',
         desc: 'oasis API 开发文档',
-        href: 'www.baidu.com'
+        href: 'http://172.16.9.7:8080/oasis/docs/html/tpl/tpl.html'
     }, {
         title: 'oasisL-1.0',
         desc: 'oasisL-1.0 API 开发文档',
-        href: 'www.baidu.com'
+        href: 'http://172.16.9.7:8080/oasisL1.0/docs/index.html'
     }, {
         title: 'oasisL-2.0',
         desc: 'oasisL-2.0 API 开发文档',
-        href: 'www.baidu.com'
+        href: 'http://172.16.9.7:8080/oasisL/docs/index.html'
     }, {
         title: 'rhyton',
         desc: 'rhyton API 开发文档',
-        href: 'www.baidu.com'
-    }, {
-        title: 'jquery',
-        desc: 'jquery API 开发文档',
-        href: 'www.baidu.com'
-    }, {
-        title: 'requirejs',
-        desc: 'requirejs API 开发文档',
-        href: 'www.baidu.com'
+        href: 'http://172.16.9.7:4000/api/doc/index.html#/demo'
     }, {
         title: 'lodash',
         desc: 'lodash API 开发文档',
-        href: 'www.baidu.com'
-    }, {
-        title: 'path',
-        desc: 'path API 开发文档',
-        href: 'www.baidu.com'
+        href: 'http://172.16.9.7:8080/download/lodashjs/#_groupbycollection-iteratee_identity-thisarg'
     }, {
         title: 'mock',
         desc: 'mock API 开发文档',
-        href: 'www.baidu.com'
+        href: 'http://172.16.9.7:8080/oasisL1.0/docs/index.html#/mock'
     }, {
-        title: 'echart',
+        title: 'echart2.0',
         desc: 'echart API 开发文档',
-        href: 'www.baidu.com'
+        href: 'http://ued.njsecnet.com:8080/css/'
     }, {
         title: 'css',
         desc: 'css API 开发文档',
@@ -56,6 +44,18 @@ const docListData = [
         title: 'less',
         desc: 'less API 开发文档',
         href: 'www.baidu.com'
+    }, {
+        title: 'jquery',
+        desc: 'jquery API 开发文档',
+        href: ''
+    }, {
+        title: 'requirejs',
+        desc: 'requirejs API 开发文档',
+        href: ''
+    }, {
+        title: 'path',
+        desc: 'path API 开发文档',
+        href: ''
     }
 ];
 
