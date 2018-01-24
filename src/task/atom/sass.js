e7a0a7df88abf4dfc3d0948415cd278f9cf1505b
wf <742406475@qq.com>
2018-01-24 16:48:57 +0800

    优化sass.js文件

--- src/task/atom/sass.js
+++ src/task/atom/sass.js
@@ -6,6 +6,7 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const compass = require('gulp-compass');
+const _ = require('lodash');
 const { isDirExist, readFistLevelFolder } = require('../utils/file');
 
 module.exports = function (config = {}, cbs = {}) {
@@ -19,10 +20,8 @@ module.exports = function (config = {}, cbs = {}) {
     } = cbs;
 
     start();
-    //  用于多模块情况下记录是不是最后sass编译完成
-    let sassNo = 0;
     // 非模块化的情况
-    if (Object.prototype.toString.call(srcBase) === '[object String]') {
+    if ( _.isString(srcBase)) {
         const stream = gulp.src(src)
             .pipe(plumber((err) => {
                 log(err);
@@ -42,27 +41,31 @@ module.exports = function (config = {}, cbs = {}) {
             });
 
         return stream;
-    }// 模块化的情况
-    for (let i = 0; i < src.length; i++) {
-        if (isDirExist(srcBase[i]) && readFistLevelFolder(srcBase[i]).length !== 0) { // 目录存在,并且目录不为空
-            gulp.src(src[i])
-                .pipe(plumber((err) => {
-                    log(err);
-                }))
-                .pipe(compass({
-                    css: destBase[i],
-                    sass: srcBase[i],
-                    image: compassSetting.imageDest,
-                    font: compassSetting.fontSrc,
-                    style: isCompress ? 'compressed' : 'compact',
-                    sourcemap: !!isOpenSourceMap
-                }))
-                .pipe(gulp.dest(destBase[i]))
-                .on('end', () => {
-                    sassNo++ === (src.length - 1) ? end() : undefined;
-                });
-        } else { // 容错,当有的文件夹sass中不存在
-            sassNo++ === (src.length - 1) ? end() : undefined;
+    }else{
+        //  用于多模块情况下记录是不是最后sass编译完成
+        let sassNo = 0;
+        // 模块化的情况
+        for (let i = 0; i < src.length; i++) {
+            if (isDirExist(srcBase[i]) && readFistLevelFolder(srcBase[i]).length !== 0) { // 目录存在,并且目录不为空
+                gulp.src(src[i])
+                    .pipe(plumber((err) => {
+                        log(err);
+                    }))
+                    .pipe(compass({
+                        css: destBase[i],
+                        sass: srcBase[i],
+                        image: compassSetting.imageDest,
+                        font: compassSetting.fontSrc,
+                        style: isCompress ? 'compressed' : 'compact',
+                        sourcemap: !!isOpenSourceMap
+                    }))
+                    .pipe(gulp.dest(destBase[i]))
+                    .on('end', () => {
+                        sassNo++ === (src.length - 1) ? end() : undefined;
+                    });
+            } else { // 容错,当有的文件夹sass中不存在
+                sassNo++ === (src.length - 1) ? end() : undefined;
+            }
         }
     }
 
